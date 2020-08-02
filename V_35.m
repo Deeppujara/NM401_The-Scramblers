9454e5be18d336c09818557648d9a0e0c69a81f5
Deep Pujara <59196192+Deeppujara@users.noreply.github.com>
2020-08-02 22:15:24 +0530

    Delete V_35.m

--- V_35.m
+++
@@ -1,9 +0,0 @@
-function [outbits,bits] = V_35(Tot_bits,e)
-    polynomial = [1 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1];
-    num_z = ceil((Tot_bits)*((1/2)+e));
-    a = ones(1,Tot_bits);
-    a(1,1:num_z) = 0;
-    bits = reshape(a(randperm(Tot_bits)),1,Tot_bits); 
-    m = length(polynomial)-1;
-    [outbits] = Scramb(polynomial,bits);
-end
